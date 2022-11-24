4f9d4af4c352b52d7b36f7c5826d8d6944309d3f
Rohini001 <118426885+Rohini001@users.noreply.github.com>
2022-11-24 23:05:47 +0530

    Add files via upload

---
+++ exception/Q1_Exception.java
@@ -0,0 +1,182 @@
+package com.example.assigment.exception;
+
+import java.util.Scanner;
+
+class Student {
+	private int rollno;
+	private String sname;
+	private String saddress;
+	private StudentResult srobj;
+
+	public Student(int rollno, String sname, String saddress, StudentResult srobj) {
+		super();
+		this.rollno = rollno;
+		this.sname = sname;
+		this.saddress = saddress;
+		this.srobj = srobj;
+	}
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getSaddress() {
+		return saddress;
+	}
+
+	public void setSaddress(String saddress) {
+		this.saddress = saddress;
+	}
+
+	public StudentResult getSrobj() {
+		return srobj;
+	}
+
+	public void setSrobj(StudentResult srobj) {
+		this.srobj = srobj;
+	}
+
+}
+
+class StudentResult {
+
+	private int marksOfSub1;
+	private int marksOfSub2;
+	private int marksOfSub3;
+	private int marksOfSub;
+
+	public StudentResult(int marksOfSub1, int marksOfSub2, int marksOfSub3, int marksOfSub) {
+		super();
+		this.marksOfSub1 = marksOfSub1;
+		this.marksOfSub2 = marksOfSub2;
+		this.marksOfSub3 = marksOfSub3;
+		this.marksOfSub = marksOfSub;
+	}
+
+	public int getMarksOfSub1() {
+		return marksOfSub1;
+	}
+
+	public void setMarksOfSub1(int marksOfSub1) {
+		this.marksOfSub1 = marksOfSub1;
+	}
+
+	public int getMarksOfSub2() {
+		return marksOfSub2;
+	}
+
+	public void setMarksOfSub2(int marksOfSub2) {
+		this.marksOfSub2 = marksOfSub2;
+	}
+
+	public int getMarksOfSub3() {
+		return marksOfSub3;
+	}
+
+	public void setMarksOfSub3(int marksOfSub3) {
+		this.marksOfSub3 = marksOfSub3;
+	}
+
+	public int getMarksOfSub() {
+		return marksOfSub;
+	}
+
+	public void setMarksOfSub(int marksOfSub) {
+		this.marksOfSub = marksOfSub;
+	}
+
+}
+
+class ResultException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ResultException(String noOfSubject) {
+		super(noOfSubject);
+	}
+}
+
+class NegtaiveMarksException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NegtaiveMarksException(String marks) {
+		super(marks);
+	}
+}
+
+class FailException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FailException(String score) {
+		super(score);
+	}
+}
+
+public class Q1_Exception {
+	private static void validate1(double score) throws FailException {
+		if (score < 40) {
+			throw new FailException("Total score is less than 40%.");
+		}
+	}
+
+	private static void validate(int m1, int m2, int m3, int m4) throws ResultException {
+		if (m1 < 30 || m2 < 30 || m3 < 30 || m4 < 30) {
+			throw new ResultException("User is failed in more than 1 subject.");
+		}
+	}
+
+	private static void validate2(int m1, int m2, int m3, int m4) throws NegtaiveMarksException {
+		if (m1 < 0 || m2 < 0 || m3 < 0 || m4 < 0) {
+			throw new NegtaiveMarksException("User mistakenly input the negative marks.");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		StudentResult sr = new StudentResult(60, 40, -10, 30);
+		Student s1 = new Student(10, "Ram", "Pune", sr);
+		try {
+			Scanner scn= new Scanner(System.in);
+			System.out.println("Enter the marks of sub1:");
+			int m1 = scn.nextInt();  sr.setMarksOfSub(m1);   //sr.getMarksOfSub(); 
+			System.out.println("Enter the marks of sub2:");
+			int m2 = scn.nextInt(); sr.setMarksOfSub1(m2);
+			System.out.println("Enter the marks of sub3:");
+			int m3 = scn.nextInt(); sr.setMarksOfSub2(m3);
+			System.out.println("Enter the marks of sub4:");
+			int m4 = scn.nextInt(); sr.setMarksOfSub3(m4);
+			
+			int total = m1 + m2 + m3 + m4;
+			double score = total / 4;
+			System.out.println("score :" + score);
+
+			validate(m1, m2, m3, m4);
+			validate1(score);
+			validate2(m1, m2, m3, m4);
+
+		} catch (FailException e) {
+			System.out.println("Exception :");
+			e.printStackTrace();
+		} catch (ResultException e) {
+			System.out.println("Exception :");
+			e.printStackTrace();
+		} catch (NegtaiveMarksException e) {
+			System.out.println("Exception :");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ exception/Q3_TryWithResources.java
@@ -0,0 +1,19 @@
+package com.example.assigment.exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Q3_TryWithResources {
+
+	public static void main(String[] args) {
+		String line;
+		try (BufferedReader br = new BufferedReader(new FileReader("Desktop\\Array_Assignment.txt"))) {
+			while ((line = br.readLine()) != null) {
+				System.out.println("Line :" + line);
+			}
+		} catch (IOException e) {
+			System.out.println("IOException in try block :" + e.getMessage());
+		}
+	}
+}
